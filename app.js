816d135830137af81fef6c80f437766dd3692eb5
Jim Jiang <jim.jiang@optus.com.au>
2020-06-09 16:14:47 +1000

    var to const

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 require('dotenv').config();
 
+const tokenRouter = require('./routes/token');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var tokenRouter = require('./routes/token');
-
-var app = express();
+const app = express();
 
 app.use('/token', tokenRouter);
 
